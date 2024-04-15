52d9827a64e64ca7f25320cacc7868086e852fc8
duda090 <128512746+duda090@users.noreply.github.com>
2024-04-15 18:39:38 -0300

    teste

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
+<?php
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -463,3 +463,4 @@
 </body>
 
 </html>
+?>
